cfe61a31ae3d53a3005dffa4e21602d0f4d4bd2e
naiomi <Naiomi.Naidoo@icloud.com>
2023-07-25 16:11:33 +0200

    Implementing AOP for logging

--- src/main/java/com/spring/aspects/CalculatorLoggingAspect.java
+++ src/main/java/com/spring/aspects/CalculatorLoggingAspect.java
@@ -28,15 +28,11 @@ public class CalculatorLoggingAspect {
         log.info("The method {}() ends", name);
     }
 
-
-
     @AfterReturning("execution(* *.*(..))")
     public void logAfterReturning(JoinPoint joinPoint) {
         var name = joinPoint.getSignature().getName();
         log.info("The method {}() ends", name);
     }
-
-
 
     //Executed only after there is an exception thrown by the join point method
     @AfterThrowing("execution(* *.*(..))")
@@ -45,7 +41,6 @@ public class CalculatorLoggingAspect {
         log.info("An exception has been thrown in {}()", name);
     }
 
-    //OR
     //The exception thrown by the join point can be accessed by adding an argument of type Exception to the advice method
     //The type Throwable is a superclass of all errors and exceptions in Java
     //The following advice will catch all exceptions thrown by the join points
@@ -55,7 +50,7 @@ public class CalculatorLoggingAspect {
         log.info("An exception {} has been thrown in {}()", ex, name);
     }
 
-    /*
+
     //OR
     //If you are interested in only 1 type of exception, you can specify the type in the advice method
     //This advice will only execute when exceptions of a compatible type are thrown
@@ -83,7 +78,16 @@ public class CalculatorLoggingAspect {
             throw e;
         }
         log.info("The method {}() ends", name);
-    }*/
+    }
 
+    @Before("execution(* *.*(..))")
+    public void logJointPoint(JoinPoint joinPoint) {
+        log.info("Joint point {}", joinPoint.getKind());
+        log.info("Signature declaring type {}", joinPoint.getSignature().getDeclaringTypeName());
+        log.info("Signature name {}", joinPoint.getSignature().getName());
+        log.info("Arguments {}", Arrays.toString(joinPoint.getArgs()));
+        log.info("Target class {}", joinPoint.getTarget().getClass().getName());
+        log.info("This class {}", joinPoint.getThis().getClass().getName());
+    }
 
 }
